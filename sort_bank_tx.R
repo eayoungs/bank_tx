43e104dfca7316b637a71fad9a1fd503a5ab9420
Eric Youngson <eric@scneco.com>
2015-12-28 20:29:28 -0800

    Revised first function to format columns in relevant R datatypes to facilitate sorting, added description header information to R module, exported R Studio prototyping data

--- sort_bank_tx.R
+++ sort_bank_tx.R
@@ -3,15 +3,29 @@
 # __copyright__ = "Copyright Dec. 2015, Succession Ecological Services"
 # __license__ = "GNU Affero (GPLv3)"
 # 
-# 
+
 
 # install.packages("lubridate") ## Comment out after first calsource()
 # library("lubridate")
 
 
 SelectCurrentMnth <- function(fname, start.date){
+  # Reads exported transaction data from Simple (online bank) and return a
+  # dataframe with correct R types for sorting
+  # 
+  # Args:
+  #   fname (String): Name of file to be processed
+  #   start.date (Date): Starting date for transactions to be subset
+  # 
+  # Returns:
+  #   A dataframe containing transaction data fro the time period specified by
+  #   parameters, with useful R datatypes for date & factors
   bank.tx = read.csv(fname, header = TRUE, sep = ",", stringsAsFactors = FALSE)
   
   month.tx = bank.tx[which(bank.tx$Date >= start.date), ]
+  month.tx$Date = as.Date(month.tx$Date)
+  month.tx$Activity = factor(month.tx$Activity)
+  month.tx$Category = factor(month.tx$Category)
+
   return(month.tx)
 }
